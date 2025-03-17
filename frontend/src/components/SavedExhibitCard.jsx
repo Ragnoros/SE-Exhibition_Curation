8a977edc3686d818ae0739994998e51171cff12c
Lorontal <137430073+Lorontal@users.noreply.github.com>
2025-03-17 10:02:56 +0000

    got saved exhibits displaying

---
+++ frontend/src/components/SavedExhibitCard.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  Card,
+  Box,
+  CardMedia,
+  Typography,
+  CardContent,
+  Tooltip,
+  Chip,
+} from "@mui/material";
+import { exhibitStyles } from "../css/ExhibitCard.styles";
+
+function SavedExhibitCard({ exhibit }) {
+  return (
+    <Card sx={exhibitStyles.card}>
+      {" "}
+      <Box sx={exhibitStyles.mediaContainer}>
+        <CardMedia
+          component="img"
+          sx={exhibitStyles.cardMedia}
+          image={exhibit._images?._primary_thumbnail || noImage}
+          title={exhibit.objectType}
+        />
+      </Box>
+      <CardContent sx={exhibitStyles.cardContent}>
+        <Box sx={exhibitStyles.contentWrapper}>
+          <Tooltip title={exhibit.objectType} placement="top">
+            <Typography sx={exhibitStyles.title} variant="h6" component="h2">
+              {exhibit.objectType}
+            </Typography>
+          </Tooltip>
+        </Box>
+        <Box sx={exhibitStyles.infoRow}>
+          {exhibit._primaryDate && (
+            <Chip
+              size="small"
+              label={`Created: ${new Date(exhibit._primaryDate).getFullYear()}`}
+              sx={exhibitStyles.infoChip}
+            />
+          )}
+          <Chip
+            size="small"
+            label={`Creator: ${exhibit._primaryMaker.name}`}
+            sx={exhibitStyles.infoChip}
+          />
+        </Box>
+        <Box sx={exhibitStyles.chipContainer}>
+          <Chip
+            size="small"
+            label={exhibit._currentLocation.displayName}
+            sx={exhibitStyles.chip}
+          />
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default SavedExhibitCard;
--- frontend/src/components/SavedExhibits.jsx
+++ frontend/src/components/SavedExhibits.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { exhibitStyles } from "../css/ExhibitCard.styles";
+import SavedExhibitCard from "./SavedExhibitCard";
+import { Box } from "@mui/material";
 
 const SavedExhibits = ({ savedExhibits }) => {
   return (
-    <div>
-      temphhohihoigouyguifiuyfiyfyifiyfiyfvjhvkufiufguguyuogougouguobjhbbklbliyuluigougyufufuifiuh
-      {console.log(savedExhibits)}
-    </div>
+    <>
+      <Box sx={exhibitStyles.mainContent}>
+        <Box sx={exhibitStyles.grid}>
+          {savedExhibits.map((exhibit) => (
+            <SavedExhibitCard
+              key={savedExhibits.systemNumber}
+              exhibit={exhibit}
+            />
+          ))}
+        </Box>
+      </Box>
+    </>
   );
 };
 
